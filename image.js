40ba747a531cdea980e911dd3049950a37d37cb4
kyllian <kyllian007@gmail.com>
2022-06-07 12:08:40 +0200

    Fixed issues with file loading

--- image.js
+++ image.js
@@ -30,25 +30,24 @@ exports.createImage = async (apiResponse) => {
             context.fillStyle = color.darkColor(shortPants);
             context.fillText("Kan ik vandaag een korte \nbroek aan?", 64, 128);
     
-            const weatherImage = await loadImage(__dirname + "/assets/" + shortPants ? "./assets/yes-man" : "./assets/no-man");
-            //const weatherImage = await loadImage(shortPants ? "./assets/yes-man.png" : "./assets/no-man.png");
+            const weatherImage = await loadImage(__dirname + "/assets/" + (shortPants ? "/yes-man" : "/no-man") + ".png");
             const widthAspect = weatherImage.width > weatherImage.height;
             const desiredWidth = widthAspect ? 500 : 500 * (500 / weatherImage.height);
             const desiredHeight = widthAspect ? 500 * (desiredWidth / weatherImage.width) : 500;
             context.drawImage(weatherImage, (canvas.width / 2) - (desiredWidth / 2), (canvas.height / 2) - (desiredHeight / 2) + midOffset, desiredWidth, desiredHeight);
     
-            const locationIcon = await loadImage(`./assets/location_${shortPants ? "orange" : "blue"}.png`);
+            const locationIcon = await loadImage(`${__dirname}/assets/location_${shortPants ? "orange" : "blue"}.png`);
             context.drawImage(locationIcon, 64, informationHeight, iconWidth, iconHeight);
     
             context.font = "32px Roboto";
             context.fillText(apiResponse.location, 128, informationHeight + 36);
     
-            const rainIcon = await loadImage(`./assets/drop_${shortPants ? "orange" : "blue"}.png`);
+            const rainIcon = await loadImage(`${__dirname}/assets/drop_${shortPants ? "orange" : "blue"}.png`);
             context.drawImage(rainIcon, 64, informationHeight + (heightDifference + iconHeight), iconWidth, iconHeight);
     
             context.fillText(`${apiResponse.rainChance}% kans`, 128, informationHeight + (heightDifference + iconHeight) + 36);
     
-            const temperatureIcon = await loadImage(`./assets/temperature_${shortPants ? "orange" : "blue"}.png`);
+            const temperatureIcon = await loadImage(`${__dirname}/assets/temperature_${shortPants ? "orange" : "blue"}.png`);
             context.drawImage(temperatureIcon, 64, informationHeight + (heightDifference + iconHeight) * 2, iconWidth, iconHeight);
     
             context.fillText(`${apiResponse.temperature}°C`, 128, informationHeight + (heightDifference + iconHeight) * 2 + 36);
